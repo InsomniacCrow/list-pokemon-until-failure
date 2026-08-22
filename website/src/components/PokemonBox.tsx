409fad5085718549be71238abbf5b8af2cbb51fb
InsomniacCrow <143170718+InsomniacCrow@users.noreply.github.com>
2026-08-22 15:05:44 +1000

    fix: sizing real quick bc it was pissing me off

--- website/src/components/PokemonBox.tsx
+++ website/src/components/PokemonBox.tsx
@@ -1,7 +1,7 @@
 export default function ({ number, name }: { number: string; name: string }) {
   return (
     <div className="px-2 py-2">
-      <div className="border w-32 h-32 rounded-md flex flex-col justify-between pb-4">
+      <div className="border w-34 h-34 rounded-md flex flex-col justify-between pb-4">
         <p className="text-sm p-2 font-silk opacity-50">#{number}</p>
         <div className="p-2 text-xs">placeholder img</div>
         <div className="w-full flex items-center justify-center">
